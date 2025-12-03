4292e237b6615597f7695198364c0fda23875ed0
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-12-03 14:44:04 +0100

    feat: App.jsx

--- react-test-project/src/index.js
+++ react-test-project/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './components/navbar';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Product from './components/product';
+import App from './App';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Product/>);
+root.render(<App/>);
