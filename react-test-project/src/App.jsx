12c9385e5982f836b79ad5b94d977af4de309579
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-12-04 10:45:42 +0100

    feat: Data transferred to shopping cart

--- react-test-project/src/App.jsx
+++ react-test-project/src/App.jsx
@@ -4,19 +4,43 @@ import Product from "./components/product";
 import ShoppingCart from "./components/shopping-cart";
 
 class App extends Component {
-  state = {};
+  state = {
+    items: [],
+  };
+  addItem = (amount, name, price) => {
+    let currentItems = this.state.items;
+    currentItems.push({ amount, name, price });
+    this.setState({ items: currentItems });
+    console.log(this.state);
+  };
   render() {
     return (
       <React.Fragment>
         <Navbar />
         <div className="main-container">
           <div className="product-container">
-            <Product title="Tomaten" description="Text über Tomaten" />{" "}
-            <Product title="Gurken" description="Text über Gurken" />{" "}
-            <Product title="Karotten" description="Text über Karotten" />{" "}
-            <Product title="Paprika" description="Text über Paprika" />
+            <Product
+              onAdd={() => this.addItem(1, "Tomaten", 2.5)}
+              title="Tomaten"
+              description="Text über Tomaten"
+            />{" "}
+            <Product
+              onAdd={() => this.addItem(1, "Gurken", 3.0)}
+              title="Gurken"
+              description="Text über Gurken"
+            />{" "}
+            <Product
+              onAdd={() => this.addItem(1, "Karotten", 1.5)}
+              title="Karotten"
+              description="Text über Karotten"
+            />{" "}
+            <Product
+              onAdd={() => this.addItem(1, "Paprika", 2.0)}
+              title="Paprika"
+              description="Text über Paprika"
+            />
           </div>
-          <ShoppingCart />
+          <ShoppingCart items={this.state.items} />
         </div>
       </React.Fragment>
     );
