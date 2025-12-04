b76303e936ae0266febc9eeec7244bb9233a2dc7
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-12-04 10:07:52 +0100

    feat: render shopping cart

--- react-test-project/src/components/shopping-cart.jsx
+++ react-test-project/src/components/shopping-cart.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ShoppingCart extends Component {
-  state = {};
+  state = {
+    items: [
+      { name: "Tomaten", amount: 1, price: 2.5 },
+      { name: "Äpfel", amount: 3, price: 3.5 },
+    ],
+  };
   render() {
     return (
       <div className="shopping-cart">
         <h2>Warenkorb</h2>
+        {this.state.items.map(item => <div key={item.name}>{item.amount}x {item.name} {item.price}€</div>)}
       </div>
     );
   }
